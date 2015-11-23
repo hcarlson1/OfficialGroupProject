c80030241c6e790f6297f11de25e52de1cb3ac07
heather <hcarlson1@css.edu>
2015-11-23 13:24:45 -0600

    Added game base

---
+++ Team Project/Assets/Scripts/Pickup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public int numberOfCoins = 1;
+
+	private int count;
+	
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("coin"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+		
+	}
+	
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= numberOfCoins)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
---
+++ Team Project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public int speed = 2;
+	
+	void Update () {
+		transform.Rotate (new Vector3 (0, 15, 0) * Time.deltaTime * speed);
+	}
+}
